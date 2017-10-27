adca0a15ce8f351b3f1b8ac25fab56fc59a0c45b
julio.quiroga <quirogajulio27@gmail.com>
2017-10-27 15:06:22 -0300

    nueva vista oferta

--- app/src/main/java/com/teachnow/com/teachnow/MainActivity.java
+++ app/src/main/java/com/teachnow/com/teachnow/MainActivity.java
@@ -2,6 +2,11 @@ package com.teachnow.com.teachnow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LayoutInflater layoutInflater = LayoutInflater.from(getBaseContext());
+        View viewOferta = layoutInflater.inflate(R.layout.activity_oferta, null, false);
+
+
+        LinearLayout pantallaOfrta = findViewById(R.id.jobs_container);
+        pantallaOfrta.addView(viewOferta);
+
+        viewOferta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), "Oferta", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
