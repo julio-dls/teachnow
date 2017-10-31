46944a455051d27bf74f699e08916e3453de6ae8
julio.quiroga <quirogajulio27@gmail.com>
2017-10-31 18:36:47 -0300

    mejora de la vista oferta

--- app/src/main/java/com/teachnow/com/teachnow/Oferta.java
+++ app/src/main/java/com/teachnow/com/teachnow/Oferta.java
@@ -2,6 +2,7 @@ package com.teachnow.com.teachnow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Oferta extends AppCompatActivity {
 
