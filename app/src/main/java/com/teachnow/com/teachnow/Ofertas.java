42f5519db08113c2cd75cfd7d0f83d96df0d273a
MiguelTerre <miguel.duenasterre@davinci.edu.ar>
2017-12-02 17:42:05 -0300

    Pasa de un elemento de listview Adjuntar_CV

--- app/src/main/java/com/teachnow/com/teachnow/Ofertas.java
+++ app/src/main/java/com/teachnow/com/teachnow/Ofertas.java
@@ -4,13 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ListView;
-
+import android.widget.AdapterView;
+import android.content.Intent;
+import android.widget.AdapterView.OnItemClickListener;
+import android.view.View;
 import com.teachnow.com.teachnow.controller.Controller;
 
 public class Ofertas extends AppCompatActivity {
     private EditText editText;
     private ListView listView;
     private Controller controller;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +25,21 @@ public class Ofertas extends AppCompatActivity {
         String palabraClave = getIntent().getExtras().getString("palabra_clave");
         listView = (ListView) findViewById(R.id.listaOfertas);
         listView.setAdapter(new EmpleoAdapter(getBaseContext(), controller.showEverything(palabraClave)));
+
+        listView.setOnItemClickListener(new ListClickHandler());
     }
+        public class ListClickHandler implements OnItemClickListener{
+
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long arg3) {
+                // TODO Auto-generated method stub
+
+                Intent intent = new Intent(Ofertas.this, Activity_Postulacion.class);
+                //intent.putExtra("selected-item", text);
+                startActivity(intent);
+
+            }
+
+        }
+
 }
