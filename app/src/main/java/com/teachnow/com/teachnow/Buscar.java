23a04d10755fabcb7b5a2ae22d27f4e4134c201e
julio.quiroga <quirogajulio27@gmail.com>
2017-12-18 20:45:40 -0300

    agregue filtro a activity buscar

--- app/src/main/java/com/teachnow/com/teachnow/Buscar.java
+++ app/src/main/java/com/teachnow/com/teachnow/Buscar.java
@@ -6,8 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -15,7 +17,9 @@ import com.teachnow.com.teachnow.controller.Controller;
 import com.teachnow.com.teachnow.dominio.Empleo;
 
 import java.util.List;
-
+//https://developer.android.com/guide/topics/ui/controls/togglebutton.html
+//https://developer.android.com/guide/topics/ui/controls/radiobutton.html
+//https://developer.android.com/guide/topics/ui/controls/checkbox.html
 public class Buscar extends AppCompatActivity {
 
     private Button buscar;
@@ -50,5 +54,44 @@ public class Buscar extends AppCompatActivity {
                 startActivity(ofts);
             }
         });
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch (view.getId()) {
+            case R.id.checkbox_meat:
+                if (checked) {
+                    // Put some meat on the sandwich
+                } else {
+                    // Remove the meat
+                }
+                break;
+            case R.id.checkbox_cheese:
+                if (checked) {
+                    // Cheese me
+                } else {
+                    // I'm lactose intolerant
+                }
+                break;
+        }
+    }
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_pirates:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.radio_ninjas:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
     }
 }
